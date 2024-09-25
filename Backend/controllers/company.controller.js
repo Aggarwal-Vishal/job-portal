dbbeb2e9219f6f424921a995621706e7d8909293
Vishal Aggarwal <007vishal.aggarwal@gmail.com>
2024-09-25 10:26:05 +0530

    building backend

---
+++ Backend/controllers/company.controller.js
@@ -0,0 +1,36 @@
+import company from "../models/company.model.js";
+
+export const registerCompany = async (req, res) => {
+  try {
+    const { companyName } = req.body;
+    if (!companyName) {
+      return res.status(400).json({
+        message: "Company name is required",
+        success: false,
+      });
+    }
+    let company = await company.findOne({ name: companyName });
+    if (company) {
+      return res.status(400).json({
+        message: "You can't register same company",
+        success: false,
+      });
+    }
+    company = await company.create({
+      name: companyName,
+      userId: req.id,
+    });
+    return res.status(201).json({
+      message: "Company registered successfully",
+      company,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCompany = async (req, res) => {
+  try {
+  } catch (error) {}
+};
---
+++ Backend/models/company.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const company = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+    },
+    website: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    logo: {
+      type: String,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
