f9b2e22dd07cff8f4ba3e92e7f8a2035009fe756
Vishal Aggarwal <007vishal.aggarwal@gmail.com>
2024-10-09 22:18:10 +0530

    Added more features

--- Frontend/src/components/Profile.jsx
+++ Frontend/src/components/Profile.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import Navbar from "./ui/shared/Navbar";
@@ -9,11 +10,14 @@ import { Badge } from "./ui/badge";
 import { Label } from "./ui/label";
 import AppliedJobTable from "./AppliedJobTable";
 import UpdateProfileDialog from "./UpdateProfileDialog";
+import { useSelector } from "react-redux";
 
-const Skills = ["html", "css", "javascript", "ReactJs"];
+// const Skills = ["html", "css", "javascript", "ReactJs"];
 const isResume = true;
 const Profile = () => {
   const [open, setOpen] = useState(false);
+
+  const { user } = useSelector((store) => store.auth);
   return (
     <div>
       <Navbar />
@@ -24,11 +28,8 @@ const Profile = () => {
               <AvatarImage src={LOGO} alt="profile" />
             </Avatar>
             <div>
-              <h1 className="font-medium text-xl">Full Name</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Perspiciatis, earum.
-              </p>
+              <h1 className="font-medium text-xl">{user?.fullname}</h1>
+              <p>{user?.profile?.bio}</p>
             </div>
           </div>
           <Button
@@ -42,18 +43,20 @@ const Profile = () => {
         <div className="p-4 space-y-2">
           <div className="flex items-center gap-3">
             <Mail />
-            <span>test@gmail.com</span>
+            <span>{user?.email}</span>
           </div>
           <div className="flex items-center gap-3">
             <Contact />
-            <span>1234567890</span>
+            <span>{user?.phoneNumber}</span>
           </div>
         </div>
         <div>
           <h1 className="font-bold text-lg my-4">Skills</h1>
           <div className="flex items-center gap-2">
-            {Skills.length !== 0 ? (
-              Skills.map((item, index) => <Badge key={index}>{item}</Badge>)
+            {user?.profile?.skills.length !== 0 ? (
+              user?.profile?.skills.map((item, index) => (
+                <Badge key={index}>{item}</Badge>
+              ))
             ) : (
               <span>NA</span>
             )}
@@ -64,10 +67,10 @@ const Profile = () => {
           {isResume ? (
             <a
               target="blank"
-              href="https://youtube.com/"
+              href={user?.profile?.resume}
               className="text-blue-500 hover: underline-offset-2"
             >
-              Vishal Aggarwal
+              {user?.profile?.resumeOriginalName}
             </a>
           ) : (
             <span>NA</span>
--- Frontend/src/components/UpdateProfileDialog.jsx
+++ Frontend/src/components/UpdateProfileDialog.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -12,155 +13,185 @@ import {
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { Loader2 } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { USER_API_END_POINT } from "@/utils/constant";
+import axios from "axios";
+import { setUser } from "@/redux/authSlice";
+import { toast } from "sonner";
 
 const UpdateProfileDialog = ({ open, setOpen }) => {
-  //   return (
-  // <div>
-  //   <Dialog open={open}>
-  //     <DialogContent
-  //       className="sm:max-w-[425px]"
-  //       onInteractOutside={() => setOpen(false)}
-  //     >
-  //       <DialogHeader>
-  //         <DialogTitle>Edit Profile</DialogTitle>
-  //       </DialogHeader>
-  //       <form>
-  //         <div className=" grid gap-4 py-4 ">
-  //           <div className="grid grid-cols-4 items-center gap-4">
-  //             <Label htmlFor="name" classname="text-right">
-  //               Name
-  //             </Label>
-  //             <Input id="name" name="name" classname="" />
-  //           </div>
-  //           <div className="grid grid-cols- items-center gap-4">
-  //             <Label htmlFor="bio" classname="text-right">
-  //               Bio
-  //             </Label>
-  //             <Input id="bio" name="bio" classname="" />
-  //           </div>
-  //           <div className="grid grid-cols- items-center gap-4">
-  //             <Label htmlFor="email" classname="text-right">
-  //               E-Mail
-  //             </Label>
-  //             <Input id="email" name="email" classname="" />
-  //           </div>
-  //           <div className="grid grid-cols- items-center gap-4">
-  //             <Label htmlFor="name" classname="text-right">
-  //               Phone Number
-  //             </Label>
-  //             <Input id=" PhoneNumber" name=" PhoneNumber" classname="" />
-  //           </div>
-  //           <div className="grid grid-cols- items-center gap-4">
-  //             <Label htmlFor="Skills" classname="text-right">
-  //               Skills
-  //             </Label>
-  //             <Input id="Skills" name="Skills" classname="" />
-  //           </div>
-  //           <div className="grid grid-cols- items-center gap-4">
-  //             <Label htmlFor="file" classname="text-right">
-  //               Resume
-  //             </Label>
-  //             <Input
-  //               id="file"
-  //               name="file"
-  //               type="file"
-  //               accept="application/pdf"
-  //               classname=""
-  //             />
-  //           </div>
-  //         </div>
-  //       </form>
+  const [loading, setLoading] = useState(false);
+  const { user } = useSelector((store) => store.auth);
 
-  //     </DialogContent>
-  //   </Dialog>
-  // </div>
+  const [input, setInput] = useState({
+    fullname: user?.fullname || "",
+    email: user?.email || "",
+    phoneNumber: user?.phoneNumber || "",
+    bio: user?.profile?.bio || "",
+    skills: user?.profile?.skills?.map((skill) => skill) || "",
+    file: user?.profile?.resume || "",
+  });
+
+  const dispatch = useDispatch();
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const fileChangeHandler = (e) => {
+    const file = e.target.files?.[0];
+    setInput({ ...input, file });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("fullname", input.fullname);
+    formData.append("email", input.email);
+    formData.append("phoneNumber", input.phoneNumber);
+    formData.append("bio", input.bio);
+    formData.append("skills", input.skills);
+    if (input.file) {
+      formData.append("file", input.file);
+    }
+
+    try {
+      setLoading(true);
+      const res = await axios.post(
+        `${USER_API_END_POINT}/profile/update`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          withCredentials: true,
+        }
+      );
+      if (res.data.success) {
+        dispatch(setUser(res.data.user));
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    } finally {
+      setLoading(false);
+    }
+    setOpen(false);
+    console.log(input);
+  };
+
   return (
-    <Dialog open={open}>
-      <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you&apos;re done.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              defaultValue="Pedro Duarte"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              E-Mail
-            </Label>
-            <Input
-              id="username"
-              defaultValue="@peduarte"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Bio
-            </Label>
-            <Input
-              id="username"
-              defaultValue="@peduarte"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Phone Number
-            </Label>
-            <Input
-              id="username"
-              defaultValue="@peduarte"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Skills
-            </Label>
-            <Input
-              id="username"
-              defaultValue="@peduarte"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="file" className="text-right">
-              Resume
-            </Label>
-            <Input
-              id="username"
-              defaultValue="@peduarte"
-              type="file"
-              accept="application/pdf"
-              className="col-span-3"
-            />
-          </div>
-        </div>
-        <DialogFooter className="grid place-items-center">
-          <Button type="submit" className="">
-            Save changes
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+    <div>
+      <Dialog open={open}>
+        <DialogContent
+          className="sm:max-w-[425px]"
+          onInteractOutside={() => setOpen(false)}
+        >
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+          <form onSubmit={submitHandler}>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input
+                  id="name"
+                  name="name"
+                  type="text"
+                  value={input.fullname}
+                  onChange={changeEventHandler}
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  E-Mail
+                </Label>
+                <Input
+                  id="username"
+                  name="email"
+                  type="email"
+                  value={input.email}
+                  onChange={changeEventHandler}
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Bio
+                </Label>
+                <Input
+                  id="username"
+                  name="bio"
+                  value={input.bio}
+                  onChange={changeEventHandler}
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Phone Number
+                </Label>
+                <Input
+                  id="username"
+                  name="phoneNumber"
+                  value={input.phoneNumber}
+                  onChange={changeEventHandler}
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Skills
+                </Label>
+                <Input
+                  id="username"
+                  name="skills"
+                  value={input.skills}
+                  onChange={changeEventHandler}
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="file" className="text-right">
+                  Resume
+                </Label>
+                <Input
+                  id="file"
+                  name="file"
+                  type="file"
+                  accept="application/pdf"
+                  onChange={fileChangeHandler}
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+
+            <DialogFooter>
+              {loading ? (
+                <Button className="w-full my-4">
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  Loading...
+                </Button>
+              ) : (
+                <Button type="submit" className="w-full my-4">
+                  Update
+                </Button>
+              )}
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 };
-
-//   );
-// };
 
 export default UpdateProfileDialog;
