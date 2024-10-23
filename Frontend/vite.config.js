0bba53c39f142a7d72370abad46b8740ad261f14
Vishal Aggarwal <007vishal.aggarwal@gmail.com>
2024-10-23 16:17:01 +0530

    another changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,11 +2,14 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// Define __dirname using import.meta.url
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"), // Set alias for src
     },
   },
 });
