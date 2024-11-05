d0d142757a7ebb3ce5941c3eb4f539ef84f42a7e
Vishal Aggarwal <007vishal.aggarwal@gmail.com>
2024-11-05 16:01:58 +0530

    url changed

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,11 +1,14 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
+export const USER_API_END_POINT =
+  "https://job-portal-xy40.onrender.com/api/v1/user";
 
 export const LOGO =
   "https://images.seeklogo.com/logo-png/53/1/hot-wheels-treasure-hunt-logo-png_seeklogo-532806.png";
 
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+export const JOB_API_END_POINT =
+  "https://job-portal-xy40.onrender.com/api/v1/job";
 
 export const APPLICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/application";
+  "https://job-portal-xy40.onrender.com/api/v1/application";
 
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+export const COMPANY_API_END_POINT =
+  "https://job-portal-xy40.onrender.com/api/v1/company";
